a38aa4c9e282d6c4d8229a959f2dc3ab3e7dfb28
Paolo <Paolo>
2022-01-23 22:52:51 -0500

    Advances3

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -18,8 +18,17 @@ router.post("/",
     validatesFields,
 userService.createuser);
 
+// Get user's products
 router.get("/:id/products", userService.getProductsByUserId);
 
+// Update user's info
+router.put("/:id", userService.updateUser);
+
+// Get all users
+router.get("/", userService.getUsers);
+
+// Get user's info
+router.get("/:id", userService.getUserInfo);
 
 
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,20 +2,30 @@ const User = require("../models/User");
 const Product = require("../models/Product");
 const bcryptjs = require("bcryptjs");
 const {generateJWT} = require("../helpers/generate-jwt");
+const cloudinary = require("cloudinary").v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
 
 
 exports.createuser = async (req, res) => {  
     const user = req.body;
     const {email, name} = user;
+
+    // Password
     const salt = bcryptjs.genSaltSync(10);
     user.password = bcryptjs.hashSync(user.password, salt);
 
+    // Upload user's image
+    const {tempFilePath} = req.files.file;
+    const {secure_url} = await cloudinary.uploader.upload(tempFilePath);
+    user.img = secure_url;
+
     try{
         await User.create(user);
-        const {_id} = await User.findOne({email});
+        const {_id, img} = await User.findOne({email});
         const data = {
             _id,
-            name
+            name,
+            img
             
         }
         const token = await generateJWT(_id);
@@ -46,5 +56,56 @@ exports.getProductsByUserId = async (req, res) => {
         return res.status(401).json({
             msg: error
         })
+    }
+}
+
+
+exports.updateUser = async (req, res) => {
+    console.log(req.body);
+    const {id} = req.params;
+    // console.log(req.files);
+    if(req.files){
+        const {tempFilePath} = req.files.file;
+        const {secure_url} = await cloudinary.uploader.upload(tempFilePath);
+        // product.img = secure_url;
+        req.body.img = secure_url;
+    }
+    // console.log(req.body.img);
+    try{
+        const user = await User.findByIdAndUpdate({_id: id}, req.body); 
+        const newUser = await User.findById(id);
+        res.status(202).json({
+            "message": "Profile updated successfully",
+            newUser
+        })
+    }
+    catch(error){
+        return res.status(400).json({
+            msg: error
+        })
+    }
+
+}
+
+exports.getUsers = async (req, res) => {
+    try{
+        const data = await User.find();
+        return res.status(200).json(data);
+    }
+    catch(error){
+        return res.status(400).json({
+            msg: error
+        })
+    }
+}
+
+exports.getUserInfo = async(req, res) => {
+    const {id} = req.params;
+    try{
+        const data = await User.findById(id);
+        return res.status(200).json(data)
+    }
+    catch(error){
+        return res.status(400).json(error);
     }
 }
